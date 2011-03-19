967ce269efdfc92f732e2801096041ad315fba51
Sylvain PRAS <sylvain.pras@addonline.fr>
2011-03-19 17:28:05 +0100

    ajout module Category admin product tab : edit product link

---
+++ server/app/code/local/PoleOuest/Adminhtml/Block/Catalog/Category/Tab/Product.php
@@ -0,0 +1,31 @@
+<?
+class PoleOuest_Adminhtml_Block_Catalog_Category_Tab_Product extends Mage_Adminhtml_Block_Catalog_Category_Tab_Product
+{
+
+  protected function _prepareColumns()
+    {
+        $cols=parent::_prepareColumns();
+//erwan
+        $this->addColumn('action',
+            array(
+                'header'    => Mage::helper('catalog')->__('Action'),
+                'width'     => '50px',
+                'type'      => 'action',
+                'getter'     => 'getId',
+                'actions'   => array(
+                    array(
+                        'caption' => Mage::helper('catalog')->__('Edit'),
+                        'url'     => array(
+                            'base'=>'*/catalog_product/edit',
+                            'params'=>array('store'=>$this->getRequest()->getParam('store'))
+                        ),
+                        'field'   => 'id'
+                    )
+                ),
+                'filter'    => false,
+                'sortable'  => false,
+                'index'     => 'stores',
+        ));
+        return $cols;
+    }
+}
