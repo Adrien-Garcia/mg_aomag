e72583630112fec05ef16cb2efb0fcd64a2b14e1
Sylvain PRAS <sylvain.pras@addonline.fr>
2011-03-25 16:12:42 +0100

    modification module simple configurable product pour compatibilité magen to 1.5

--- sql/environnement.sql
+++ sql/environnement.sql
@@ -2,9 +2,17 @@
 use aomagento;
 
 -- de local vers préprod
-UPDATE core_config_data SET value='http://dam.preprod.addonline.biz/' WHERE value='http://aomagento.addonline.devl/';
-UPDATE cms_page SET content=REPLACE(content,'http://aomagento.addonline.devl/','http://aomagento.preprod.addonline.biz/');
-UPDATE cms_block SET content=REPLACE(content,'http://aomagento.addonline.devl/','http://aomagento.preprod.addonline.biz/');
+UPDATE core_config_data SET value='http://magento.addonline.biz/' WHERE value='http://aomagento.addonline.devl/';
+UPDATE cms_page SET content=REPLACE(content,'http://aomagento.addonline.devl/','http://magento.addonline.biz/');
+UPDATE cms_block SET content=REPLACE(content,'http://aomagento.addonline.devl/','http://magento.addonline.biz/');
+UPDATE core_config_data SET value='mail.add-online.fr' WHERE path='system/smtp/host';
+UPDATE core_config_data SET value='1' WHERE path='dev/log/active';
+-- FIN : de local vers préprod
+
+-- de préprod vers local
+UPDATE core_config_data SET value='http://aomagento.addonline.devl/' WHERE value='http://magento.addonline.biz/';
+UPDATE cms_page SET content=REPLACE(content,'http://magento.addonline.biz/','http://aomagento.addonline.devl/');
+UPDATE cms_block SET content=REPLACE(content,'http://magento.addonline.biz/','http://aomagento.addonline.devl/');
 UPDATE core_config_data SET value='mail.add-online.fr' WHERE path='system/smtp/host';
 UPDATE core_config_data SET value='1' WHERE path='dev/log/active';
 -- FIN : de local vers préprod
