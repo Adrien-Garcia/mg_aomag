b1b48b9e0e2d7cf4d8084a0f3b497850a02f8619
Sylvain PRAS <sylvain.pras@addonline.fr>
2011-10-25 14:07:30 +0200

    correction module SEO

--- server/app/code/local/Addonline/Seo/Block/Head.php
+++ server/app/code/local/Addonline/Seo/Block/Head.php
@@ -1,27 +1,20 @@
 <?php
 
-class Addonline_Seo_Block_Head_Canonicalurl extends Mage_Core_Block_Template
-//Mage_Page_Block_Html_Head
-//Fooman_Speedster_Block_Page_Html_Head
+if (!(string)Mage::getConfig()->getModuleConfig('Fooman_Speedster')->active == 'true')
 {
-	
-    /**
-     * Add Link element to HEAD entity (Magento 1.3)
-     *
-     * @param string $rel forward link types
-     * @param string $href URI for linked resource
-     * @return Mage_Page_Block_Html_Head
-     */
-/*
-    public function addLinkRel($rel, $href)
-    {
-        $this->addItem('link_rel', $href, 'rel="' . $rel . '"');
-        return $this;
-    }
-*/
-/*
+  class Fooman_Speedster_Block_Page_Html_Head extends Mage_Page_Block_Html_Head{}
+}
+  class Addonline_Seo_Block_Head extends Fooman_Speedster_Block_Page_Html_Head
+{
 	public function getHeadUrl()
 	{
+		if( !Mage::helper('seo')->isVersionLessThan('1.4.0.0') )
+        {
+        	//à partir de la version 1.4 de magento les URLs canaonique sont gérées, à activer dans Système>Configuration>Catalogue>otimisation moteur de recherche : 
+        	// - Utiliser un lien canonique de meta tag pour les catégories
+        	// - Utiliser un lien canonique de meta tag pour les produits
+        	return false;
+		}
 		if (empty($this->_data['urlKey'])) 
 		{
 			$url = Mage::helper('core/url')->getCurrentUrl();
@@ -41,20 +34,23 @@ class Addonline_Seo_Block_Head_Canonicalurl extends Mage_Core_Block_Template
 					$headUrl .= '/';
 				}
 			}
+			//return $headUrl;
 			$this->_data['urlKey'] =$headUrl;
         }
 		
 		return $this->_data['urlKey'];
 	}
-*/
+
 	public function getHeadProductUrl()
-    {
+    {  
 		if( !Mage::helper('seo')->isVersionLessThan('1.4.0.0') )
         {
+        	//à partir de la version 1.4 de magento les URLs canaonique sont gérées, à activer dans Système>Configuration>Catalogue>otimisation moteur de recherche : 
+        	// - Utiliser un lien canonique de meta tag pour les catégories
+        	// - Utiliser un lien canonique de meta tag pour les produits
         	return false;
 		}
-            
-		if (empty($this->_data['urlKey'])) 
+    	if (empty($this->_data['urlKey'])) 
 		{
 			$product_id = $this->getRequest()->getParam('id');
 			$_item = Mage::getModel('catalog/product')->load($product_id);
@@ -66,32 +62,8 @@ class Addonline_Seo_Block_Head_Canonicalurl extends Mage_Core_Block_Template
 					$this->_data['urlKey'] .= '/';
 				}
 			}
-			
-		}
-		
-		return $this->_data['urlKey'];
-	}
 
-	public function getHeadCategoryUrl()
-    {
-    	if( !Mage::helper('seo')->isVersionLessThan('1.4.0.0') )
-        {
-        	return false;
-		}
-		
-    	if (empty($this->_data['urlKey'])) 
-		{
-			$category_id = $this->getRequest()->getParam('id');
-			$category = Mage::getModel('catalog/category')->load($category_id);
-			$this->_data['urlKey'] = $category->getUrl();
-			if (Mage::getStoreConfig('web/seo/trailingslash')) 
-			{
-				if (!preg_match('/\\.(rss|html|htm|xml|php?)$/', strtolower($this->_data['urlKey'])) && substr($this->_data['urlKey'], -1) != '/') 
-				{
-					$this->_data['urlKey'] .= '/';
-				}
-			}
-			
+			    
 		}
 		return $this->_data['urlKey'];
 	} 
