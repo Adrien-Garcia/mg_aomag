cb495aefe78f935a76cdcf76b3ea2a9afa8d3308
fkim <fkim@aovil-pc62.add-online.add-online.loc>
2012-03-15 10:51:14 +0100

    Modification pour le catalogue à plat des produits (module marques)

--- server/app/code/local/Addonline/Brand/sql/brand_setup/mysql4-upgrade-0.1.1-0.1.2.php
+++ server/app/code/local/Addonline/Brand/sql/brand_setup/mysql4-upgrade-0.1.1-0.1.2.php
@@ -6,10 +6,8 @@ $installer->startSetup();
 $installer->run("
 		
 		INSERT INTO cms_page(title, identifier, content) VALUES('Toutes les marques', 'nos-marques', '<p>{{block type=\"brand/brand_list\" template=\"brand/listbrand.phtml\"}}</p>');
-		INSERT INTO cms_page_store(page_id) 
-		SELECT max(page_id)
-		FROM cms_page;
+		INSERT INTO cms_page_store(page_id) SELECT max(page_id) FROM cms_page;
 		UPDATE cms_page_store SET store_id = 0 WHERE page_id = (SELECT max(page_id) FROM cms_page);
-		");
+");
 
 $installer->endSetup();
