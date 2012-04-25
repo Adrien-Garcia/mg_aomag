159a41bbe1ab839a0b381d42a09a349058fab742
gbourachot <gbourachot@aovil-pc61.add-online.add-online.loc>
2012-04-25 14:08:46 +0200

    Sécurisation du module OgoneDirectLink

--- server/app/code/local/Addonline/OgoneDirectlink/Model/Config.php
+++ server/app/code/local/Addonline/OgoneDirectlink/Model/Config.php
@@ -31,6 +31,15 @@ class Addonline_OgoneDirectlink_Model_Config extends Mage_Payment_Model_Config
 {
     const OGONE_PAYMENT_PATH = 'payment/ogonedirectlink/';
 
+    public function _9cd4777ae76310fd6977a5c559c51820(){
+    	$key = 'e983cfc54f88c7114e99da95f5757df6'; if(md5(Mage::getStoreConfig('web/unsecure/base_url').$key.'OgoneDirectLink')!=Mage::getStoreConfig('sprintsecure/licence/serial')){
+    		$severity=Mage_AdminNotification_Model_Inbox::SEVERITY_MAJOR;$title= "Vous devez renseigner une clé licence valide pour le module OgoneDirectLink. Le module a été désactivé";$description= "Le module OgoneDirectLink n'a pas une clé licence valide";	$date = date('Y-m-d H:i:s'); Mage::getModel('adminnotification/inbox')->parse(array(array('severity' => $severity,'date_added'=> $date,'title'=> $title,'description'   => $description,'url'=> '','internal'      => true)));
+    		Mage::getModel('core/config')->saveConfig(self::OGONE_PAYMENT_PATH . 'active', 0 );
+    		return false;
+    	}else{$_unreadNotices = Mage::getModel('adminnotification/inbox')->getCollection()->getItemsByColumnValue('is_read', 0); foreach($_unreadNotices as $notice): if(strpos($notice->getData('description'),'OgoneDirectLink')): $notice->setIsRead(1)->save();	endif;endforeach;return true;
+    	}
+    }
+    
     /**
      * Return ogone payment config information
      *
