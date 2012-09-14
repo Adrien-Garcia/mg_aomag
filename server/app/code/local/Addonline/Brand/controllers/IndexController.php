a61d571fe4a38d7f320a882f4843fd985a428901
Sylvain PRAS <sylvain.pras@addonline.fr>
2012-09-14 16:17:48 +0200

    correction module marques

--- server/app/code/local/Addonline/Brand/controllers/IndexController.php
+++ server/app/code/local/Addonline/Brand/controllers/IndexController.php
@@ -10,6 +10,9 @@ class Addonline_Brand_IndexController extends Mage_Core_Controller_Front_Action
     public function listeAction() 
     {
    		$this->loadLayout();
+   		$headBlock = $this->getLayout()->getBlock('head');  		
+   		$headBlock->setTitle("Toutes les marques de notre site");
+   		$headBlock->setDescription("Découvrez toutes les marques de notre site");
    		$this->renderLayout();
     }
     
