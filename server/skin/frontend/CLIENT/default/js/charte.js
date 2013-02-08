9dac2993029542f744d9496a39d691202b61c8d0
lferreira <lferreira@AOVIL-PC50.add-online.add-online.loc>
2013-02-08 14:21:43 +0100

    - Ajout fonction jQuery fields, pour vidage de champs par défaut des inputs

--- server/skin/frontend/CLIENT/default/js/charte.js
+++ server/skin/frontend/CLIENT/default/js/charte.js
@@ -15,3 +15,26 @@ jQuery(function($) {
 	$('.cloud-zoom, .cloud-zoom-gallery').CloudZoom();
 	
 })
+
+/*
+ * Plugin jQuery d'initilisation des valeurs par défaut sur les champs texte
+ * Au clic, si le champ contient la valeur par défaut, celui-ci est vidé.
+ * Quand on quitte le champ, si celui-ci est vide, on remet la valeur par défaut.
+ */
+
+jQuery.fn.fields = function() {
+	this.each(function(index, element) {
+		jQuery(element).data("defaultValue", jQuery(element).val());
+		jQuery(element).focus(function() {
+			if(jQuery(this).val() == jQuery(this).data("defaultValue")) {
+				jQuery(this).val("");
+				jQuery(this).addClass('focused');
+			}
+		}).blur(function() {
+			if(jQuery(this).val() == "") {
+				jQuery(this).val(jQuery(this).data("defaultValue"));
+				jQuery(this).removeClass('focused');
+			}
+		});
+	});
+};
