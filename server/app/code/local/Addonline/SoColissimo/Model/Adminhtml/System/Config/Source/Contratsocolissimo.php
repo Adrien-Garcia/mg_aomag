6f0b55f1a17207ad3c34aceb3f849bd95a0ec085
Sylvain PRAS <sylvain.pras@addonline.fr>
2013-03-08 09:04:07 +0100

    Socolissimo suite

---
+++ server/app/code/local/Addonline/SoColissimo/Model/Adminhtml/System/Config/Source/Contratsocolissimo.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Addonline_SoColissimo
+ * 
+ * @category    Addonline
+ * @package     Addonline_SoColissimo
+ * @copyright   Copyright (c) 2013 Addonline
+ * @author 	    Addonline (http://www.addonline.fr)
+ */
+
+/**
+ * Used in creating options for Socolissimo Contract selection
+*
+*/
+class Addonline_SoColissimo_Adminhtml_Model_System_Config_Source_Contratsocolissimo
+{
+
+	/**
+	 * Options getter
+	 *
+	 * @return array
+	 */
+	public function toOptionArray()
+	{
+
+		$options = array();
+		$observer = Mage::getSingleton('socolissimo/obsserver');
+		$storeId = Mage::app()->getStore()->getId();
+		if ($observer->_9cd4777ae76310fd6977a5c559c51820($storeId, Addonline_SoColissimo_Model_Observer::FLEXIBILITE)) {
+			$options[] = array('value' => 'felxibilite', 'label'=>Mage::helper('adminhtml')->__('Flexibilité'));
+		}
+		if ($observer->_9cd4777ae76310fd6977a5c559c51820($storeId, Addonline_SoColissimo_Model_Observer::LIBERTE)) {
+			$options[] = array('value' => 'liberte', 'label'=>Mage::helper('adminhtml')->__('Liberté'));
+		}
+		return $otions;
+		
+	}
+
+
+}
---
+++ server/app/code/local/Addonline/SoColissimo/Model/Carriers/ShippingMethod.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Addonline_SoColissimo
+ * 
+ * @category    Addonline
+ * @package     Addonline_SoColissimo
+ * @copyright   Copyright (c) 2013 Addonline
+ * @author 	    Addonline (http://www.addonline.fr)
+ */
+
+class Addonline_SoColissimo_Model_Carrier_ShippingMethod
+{
+	protected $_code = 'socolissimo';
+	
+	/**
+	 * Collect rates for this shipping method based on information in $request
+	 * 
+	 * ==> les tarifs doivent être défini dans une grille ?
+	 *
+	 * @param Mage_Shipping_Model_Rate_Request $data
+	 * @return Mage_Shipping_Model_Rate_Result
+	public function collectRates(Mage_Shipping_Model_Rate_Request $request) {
+
+		$rates = parent::collectRates($request);
+		$shippingAddress = Mage::getSingleton('checkout/session')->getQuote()->getShippingAddress();		
+		
+		if ($shippingAddress && $shippingAddress->getData('soco_product_code') == 'RDV') {
+			foreach ($rates->getAllRates() as $rate) {			
+				if ($rate->getCarrier()===$this->_code) {										
+					$rate->setPrice($rate->getPrice()+(int)Mage::getStoreConfig('carriers/socolissimo/rdv_fees'));
+				}
+			}
+		}
+		if ($shippingAddress && $shippingAddress->getData('soco_product_code') == 'A2P') {
+			foreach ($rates->getAllRates() as $rate) {
+				if ($rate->getCarrier()===$this->_code) {
+					$price = $rate->getPrice()-(float)Mage::getStoreConfig('carriers/socolissimo/remise_commercant');
+					if($price < 0){ $price = 0; }
+					$rate->setPrice($price);
+				}
+			}
+		}
+		return $rates;
+	}
+	 */
+	
+	/**
+	 * Surcharge pour 
+	 *
+	 * @param Mage_Shipping_Model_Rate_Request $data
+	 * @return Mage_Shipping_Model_Rate_Result
+	 */
+	protected function _getMethodText($process, $row, $property)
+	{
+		$text = parent::_getMethodText($process, $row, $property);
+		if ($property == 'label') {
+
+			$checkoutSession = Mage::getSingleton('checkout/session');
+			$shippingData = $checkoutSession->getData('socolissimo_shipping_data');
+			if (isset($shippingData) && count($shippingData) > 0) {
+				if ($shippingData['DELIVERYMODE']=='DOM' || $shippingData['DELIVERYMODE']=='DOS') {
+		    		$text = $text.' – '.Mage::helper('socolissimo')->__("Shipping at home");
+		    	} else if ($shippingData['DELIVERYMODE']=='RDV') {
+		    		$text = $text.' – '.Mage::helper('socolissimo')->__("Making appointment");
+		    	} else if ($shippingData['DELIVERYMODE']=='BPR') {
+		    		$text = $text.' – '.Mage::helper('socolissimo')->__("Post office");
+		    	} else if ($shippingData['DELIVERYMODE']=='CIT') {
+		    		$text = $text.' – '.Mage::helper('socolissimo')->__("Cityssimo space");
+		    	} else if ($shippingData['DELIVERYMODE']=='A2P') {
+		    		$text = $text.' – '.Mage::helper('socolissimo')->__("Shop");
+		    	} 
+			}
+		}
+		return $text;
+	}
+}
+
+Z?>
