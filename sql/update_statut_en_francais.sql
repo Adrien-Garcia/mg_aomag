6ec9c6e1b0360c714e102387447a4a32299df8e6
Nicolas Tiran <nicolas.tiran@addonline.fr>
2013-06-12 14:52:42 +0200

    - sql pour statut en francais

---
+++ sql/update_statut_en_francais.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO `sales_order_status_label` (`status`, `store_id`, `label`) VALUES
+('canceled', 3, 'Annulé'),
+('closed', 3, 'Terminé'),
+('complete', 3, 'Compléter'),
+('fraud', 3, 'Fraude suspectée'),
+('holded', 3, 'En attente'),
+('pending', 3, 'En cours'),
+('pending_payment', 3, 'En attente du paiement'),
+('pending_paypal', 3, 'Paiement Paypal en cours'),
+('processing', 3, 'En cours de traitement');
