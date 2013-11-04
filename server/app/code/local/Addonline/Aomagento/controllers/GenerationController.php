88291b009c1f942bfbf1f100501f9b2ed0f206b9
Frédéric KIM <frederic.kim@addonline.fr>
2013-11-04 17:45:53 +0100

    Modification génération clé de licences pour modules AO

--- server/app/code/local/Addonline/Aomagento/controllers/GenerationController.php
+++ server/app/code/local/Addonline/Aomagento/controllers/GenerationController.php
@@ -6,10 +6,7 @@ class Addonline_Aomagento_GenerationController extends Mage_Core_Controller_Fron
 		$key = 'e983cfc54f88c7114e99da95f5757df6';
 		$hostname = isset($_GET['hostname']) ? $_GET['hostname'] : "";
 		$module = isset($_GET['module']) ? $_GET['module'] : "";
-		$id_parent = isset($_GET['id_parent']) ? $_GET['id_parent'] : "";
-		if(($module == "SoColissimoFlexibilite" && !preg_match("/_flexi$/", $id_parent)) || ($module == "SoColissimoLiberte" && !preg_match("/_liberte$/", $id_parent))) {
-			return;
-		}
+		
 		if($hostname != "") {
 			if(!preg_match("/^http:\/\/|^https:\/\//", $hostname)) {
 				$hostname = "http://".$hostname;
