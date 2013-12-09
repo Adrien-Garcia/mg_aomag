4d3f26b52194a00c5886a01de180606f22cb63cb
Guillaume Bourachot <guillaume.bourachot@addonline.fr>
2013-12-09 17:49:42 +0100

    Préparation du BO du module GLS

---
+++ server/app/code/local/Addonline/Gls/Block/Adminhtml/System/Config/Form/Field/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+class Addonline_Gls_Block_Adminhtml_System_Config_Form_Field_Config extends Mage_Adminhtml_Block_System_Config_Form_Field
+{
+	protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element) {
+
+		return $element->getElementHtml().'<a href="/doc-socolissimo-config.htm'.'" target="_blank">'.$this->__('Help to configure').'</a>';
+
+	}
+}
---
+++ server/app/code/local/Addonline/Gls/sql/gls_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Addonline_GLS
+ *
+ * @category    Addonline
+ * @package     Addonline_GLS
+ * @copyright   Copyright (c) 2013 GLS
+ * @author 	    Addonline (http://www.addonline.fr)
+ */
+
+$installer = $this;
+
+$installer->startSetup();
+
+$installer->endSetup();
