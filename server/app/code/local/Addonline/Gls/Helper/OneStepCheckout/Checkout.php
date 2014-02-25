598973ded5e31149b32d9455642b4bd8731f52ac
Guillaume Bourachot <guillaume.bourachot@addonline.fr>
2014-02-25 12:08:26 +0100

    Correction d'un problème de casse sur les chemins

--- server/app/code/local/Addonline/Gls/Helper/OneStepCheckout/Checkout.php
+++ server/app/code/local/Addonline/Gls/Helper/OneStepCheckout/Checkout.php
@@ -3,7 +3,7 @@ if ((string)Mage::getConfig()->getModuleConfig('Idev_OneStepCheckout')->active !
 {
 	class Idev_OneStepCheckout_Helper_Checkout extends Mage_Core_Helper_Abstract{}
 }
-class Addonline_GLS_Helper_OneStepCheckout_Checkout extends Idev_OneStepCheckout_Helper_Checkout
+class Addonline_Gls_Helper_OneStepCheckout_Checkout extends Idev_OneStepCheckout_Helper_Checkout
 {
 
     public function saveShipping($data, $customerAddressId)
