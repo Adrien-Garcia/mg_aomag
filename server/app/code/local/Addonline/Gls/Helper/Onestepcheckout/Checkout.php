7f1ebafa94450823df595a643ba8811b57663f6a
Guillaume Bourachot <guillaume.bourachot@addonline.fr>
2014-02-25 11:17:39 +0100

    Compatibilité avec OneStepCheckout

---
+++ server/app/code/local/Addonline/Gls/Helper/Onestepcheckout/Checkout.php
@@ -0,0 +1,13 @@
+<?php
+if ((string)Mage::getConfig()->getModuleConfig('Idev_OneStepCheckout')->active != 'true')
+{
+	class Idev_OneStepCheckout_Helper_Checkout extends Mage_Core_Helper_Abstract{}
+}
+class Addonline_GLS_Helper_OneStepCheckout_Checkout extends Idev_OneStepCheckout_Helper_Checkout
+{
+
+    public function saveShipping($data, $customerAddressId)
+    {
+        return array();
+    }
+}
