b5ed74a5b55b969fb05a2a62646cd89ade32da5b
Frédéric KIM <frederic.kim@addonline.fr>
2014-04-24 17:41:22 +0200

    Ajout d'une limite pour la comparaison des produits

---
+++ server/app/code/local/Addonline/Catalog/Model/Observer.php
@@ -0,0 +1,15 @@
+<?php
+class Addonline_Catalog_Model_Observer extends Varien_Object {
+	
+	function limitProductCompare($event) {
+		$limit = Mage::getStoreConfig('addonline/aocompare/limit');
+		
+		if (Mage::helper('catalog/product_compare')->getItemCount()<$limit) return;
+	
+		$session = Mage::getSingleton('catalog/session');
+		Mage::getSingleton('catalog/product_compare_list')->removeProduct($event->getProduct());
+	
+		$session->getMessages()->clear();
+		$session->addNotice(Mage::helper('catalog/product_compare')->__('You have reached the limit of products to compare. Remove one and try again.'));
+	}
+}
