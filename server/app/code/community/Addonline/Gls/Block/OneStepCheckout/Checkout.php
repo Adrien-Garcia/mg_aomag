c99db530e3128ad92a9f94ad960cdaaaecd23515
Guillaume Bourachot <guillaume.bourachot@addonline.fr>
2014-06-18 17:01:01 +0200

    GLS : problème de compatibilité avec OneStepCheckout

---
+++ server/app/code/community/Addonline/Gls/Block/OneStepCheckout/Checkout.php
@@ -0,0 +1,24 @@
+<?php
+class Addonline_Gls_Block_OneStepCheckout_Checkout extends Idev_OneStepCheckout_Block_Checkout
+{
+
+
+    public function differentShippingAvailable()
+    {
+
+    	// dans le cas où on livre dans un relais colis GLS, on se comporte comme si la livraison
+    	//dans une adresse différente de l'adresse de facturation n'était pas possible pour éviter d'écraser
+    	//l'addresse du relais colis qui a été enregistrée auparavent
+    	$request = Mage::app()->getRequest();
+    	$shippingMethod = $request->getParam('shipping_method');
+
+    	if (strpos($shippingMethod,'gls_relay')===0) {
+	    	$idRelais = $request->getParam('relais_socolissimo');
+	    	if ($idRelais) {
+		    	return false;
+	    	}
+    	}
+        return parent::differentShippingAvailable();
+    }
+
+}
