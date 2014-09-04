ac62878c11e67ece3869523f1698fec4600e0121
Guillaume Bourachot <guillaume.bourachot@addonline.fr>
2014-09-04 17:40:05 +0200

    Création du module GUATracker
    Signed-off-by: Guillaume Bourachot <guillaume.bourachot@addonline.fr>

---
+++ server/app/code/local/Addonline/GUATracker/Helper/Data.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package 
+ * @author Stefan richter (richter@aromicon.com)
+ * @license aromicon gmbh 2013
+ */ 
+class Addonline_GUATracker_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
