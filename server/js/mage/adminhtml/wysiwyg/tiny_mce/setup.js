b955d2e34349a1cf4fecbb9fa53c6c5af47545f2
Sylvain PRAS <sylvain.pras@addonline.fr>
2014-10-29 16:52:21 +0100

    - ajout des attibuts itemscope, itemtype et itemprop à tinyMCE

--- server/js/mage/adminhtml/wysiwyg/tiny_mce/setup.js
+++ server/js/mage/adminhtml/wysiwyg/tiny_mce/setup.js
@@ -114,6 +114,31 @@ tinyMceWysiwygSetup.prototype =
             magentowidget_url: this.config.widget_window_url,
             magentoPluginsOptions: magentoPluginsOptions,
             doctype : '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
+            /* ADDONLINE
+             * ajout des attributs itemscope, itemtype et itemprop
+             * redéfinition en passant par l'option valide_elements, extends_valide_elements ne fonctionnant pas 
+             */
+            valid_elements : "@[itemscope|itemtype|itemprop|id|class|style|title|dir<ltr?rtl|lang|xml::lang|onclick|ondblclick|"
+            	+ "onmousedown|onmouseup|onmouseover|onmousemove|onmouseout|onkeypress|"
+            	+ "onkeydown|onkeyup],a[rel|rev|charset|hreflang|tabindex|accesskey|type|"
+            	+ "name|href|target|title|class|onfocus|onblur],strong/b,em/i,strike,u,"
+            	+ "#p,-ol[type|compact],-ul[type|compact],-li,br,img[longdesc|usemap|"
+            	+ "src|border|alt=|title|hspace|vspace|width|height|align],-sub,-sup,"
+            	+ "-blockquote,-table[border=0|cellspacing|cellpadding|width|frame|rules|"
+            	+ "height|align|summary|bgcolor|background|bordercolor],-tr[rowspan|width|"
+            	+ "height|align|valign|bgcolor|background|bordercolor],tbody,thead,tfoot,"
+            	+ "#td[colspan|rowspan|width|height|align|valign|bgcolor|background|bordercolor"
+            	+ "|scope],#th[colspan|rowspan|width|height|align|valign|scope],caption,-div,"
+            	+ "-span,-code,-pre,address,-h1,-h2,-h3,-h4,-h5,-h6,hr[size|noshade],-font[face"
+            	+ "|size|color],dd,dl,dt,cite,abbr,acronym,del[datetime|cite],ins[datetime|cite],"
+            	+ "object[classid|width|height|codebase|*],param[name|value|_value],embed[type|width"
+            	+ "|height|src|*],script[src|type],map[name],area[shape|coords|href|alt|target],bdo,"
+            	+ "button,col[align|char|charoff|span|valign|width],colgroup[align|char|charoff|span|"
+            	+ "valign|width],dfn,fieldset,form[action|accept|accept-charset|enctype|method],"
+            	+ "input[accept|alt|checked|disabled|maxlength|name|readonly|size|src|type|value],"
+            	+ "kbd,label[for],legend,noscript,optgroup[label|disabled],option[disabled|label|selected|value],"
+            	+ "q[cite],samp,select[disabled|multiple|name|size],small,"
+            	+ "textarea[cols|rows|disabled|name|readonly],tt,var,big",
 
             setup : function(ed) {
                 ed.onSubmit.add(function(ed, e) {
@@ -130,6 +155,19 @@ tinyMceWysiwygSetup.prototype =
 
                 ed.onSetContent.add(function(ed, o) {
                     varienGlobalEvents.fireEvent('tinymceSetContent', o);
+                    /* ADDONLINE
+                     * ajout des attributs itemscope, itemtype et itemprop
+                     * nettoyage spécifique sur l'attribut itemscrop qui n'a pas de valeur
+                     */
+                    o.content = o.content.replace('itemscope=""', 'itemscope');
+                });
+                
+                ed.onGetContent.add(function(ed, o) {
+                	/* ADDONLINE
+                     * ajout des attributs itemscope, itemtype et itemprop
+                     * nettoyage spécifique sur l'attribut itemscrop qui n'a pas de valeur
+                     */
+                    o.content = o.content.replace('itemscope=""', 'itemscope');
                 });
 
                 ed.onSaveContent.add(function(ed, o) {
