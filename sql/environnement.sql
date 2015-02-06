c416b4053487b6ca19b159ae0c77936fbd61e8a9
Clement HORGUES <clement.horgues@addonline.fr>
2015-02-06 17:16:03 +0100

    Client 4 email + Print commande list produit

--- sql/environnement.sql
+++ sql/environnement.sql
@@ -11,10 +11,10 @@ UPDATE core_config_data SET value='1' WHERE path='dev/log/active';
 -- FIN : de local vers pr�prod
 
 -- de pr�prod vers local
-UPDATE core_config_data SET value='http://aomagento.addonline.devl/' WHERE value='http://aomagento.addonline.biz/';
-UPDATE cms_page SET content=REPLACE(content,'http://aomagento.addonline.biz/','http://aomagento.addonline.devl/');
-UPDATE cms_block SET content=REPLACE(content,'http://aomagento.addonline.biz/','http://aomagento.addonline.devl/');
-UPDATE catalog_product_entity_text SET value=REPLACE(value,'http://aomagento.addonline.biz/','http://aomagento.addonline.devl/') WHERE attribute_id IN (97,506);
+UPDATE core_config_data SET value='http://btb.laetitia.jetpulp.dev/' WHERE value='http://aomagento.clement.addonline.biz/';
+UPDATE cms_page SET content=REPLACE(content,'http://aomagento.clement.addonline.biz/','http://btb.laetitia.jetpulp.dev/');
+UPDATE cms_block SET content=REPLACE(content,'http://aomagento.clement.addonline.biz/','http://btb.laetitia.jetpulp.dev/');
+UPDATE catalog_product_entity_text SET value=REPLACE(value,'http://aomagento.clement.addonline.biz/','http://btb.laetitia.jetpulp.dev/') WHERE attribute_id IN (97,506);
 UPDATE core_config_data SET value='mail.add-online.fr' WHERE path='system/smtp/host';
 UPDATE core_config_data SET value='1' WHERE path='dev/log/active';
 -- FIN : de local vers pr�prod
