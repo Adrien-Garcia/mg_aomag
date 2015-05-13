4be33aaa62beda1f8e32cce803150b1ebe56efef
dvittorelli <delphine.vittorelli@jetpulp.fr>
2015-05-13 10:22:22 +0200

    Ajout thème CLIENT-2 avec style du header + footer

--- sql/environnement.sql
+++ sql/environnement.sql
@@ -1,20 +1,23 @@
+###################################################################
+# MERCI DE NE PAS MODIFIER LES URLS DE CE FICHIER !!!!!!!!!!!!!!!!!
+###################################################################
 
 use aomagento;
 
 -- de local vers pr�prod
-UPDATE core_config_data SET value='http://innerjoy.preprod.addonline.biz/' WHERE value='http://aomagento.clement.addonline.devl/';
-UPDATE cms_page SET content=REPLACE(content,'http://aomagento.clement.addonline.devl/','http://innerjoy.preprod.addonline.biz/');
-UPDATE cms_block SET content=REPLACE(content,'http://aomagento.clement.addonline.devl/','http://innerjoy.preprod.addonline.biz/');
-UPDATE catalog_product_entity_text SET value=REPLACE(value,'http://aomagento.clement.addonline.devl/','http://innerjoy.preprod.addonline.biz/') WHERE attribute_id IN (97,506);
+UPDATE core_config_data SET value='http://aomagento.addonline.biz/' WHERE value='http://aomagento.jetpulp.dev/';
+UPDATE cms_page SET content=REPLACE(content,'http://aomagento.jetpulp.dev/','http://aomagento.addonline.biz/');
+UPDATE cms_block SET content=REPLACE(content,'http://aomagento.jetpulp.dev/','http://aomagento.addonline.biz/');
+UPDATE catalog_product_entity_text SET value=REPLACE(value,'http://aomagento.jetpulp.dev/','http://aomagento.addonline.biz/') WHERE attribute_id IN (97,506);
 UPDATE core_config_data SET value='mail.add-online.fr' WHERE path='system/smtp/host';
 UPDATE core_config_data SET value='1' WHERE path='dev/log/active';
 -- FIN : de local vers pr�prod
 
 -- de pr�prod vers local
-UPDATE core_config_data SET value='http://btb.laetitia.jetpulp.dev/' WHERE value='http://aomagento.clement.addonline.biz/';
-UPDATE cms_page SET content=REPLACE(content,'http://aomagento.clement.addonline.biz/','http://btb.laetitia.jetpulp.dev/');
-UPDATE cms_block SET content=REPLACE(content,'http://aomagento.clement.addonline.biz/','http://btb.laetitia.jetpulp.dev/');
-UPDATE catalog_product_entity_text SET value=REPLACE(value,'http://aomagento.clement.addonline.biz/','http://btb.laetitia.jetpulp.dev/') WHERE attribute_id IN (97,506);
+UPDATE core_config_data SET value='http://aomagento.jetpulp.dev/' WHERE value='http://aomagento.addonline.biz/';
+UPDATE cms_page SET content=REPLACE(content,'http://aomagento.addonline.biz/','http://aomagento.jetpulp.dev/');
+UPDATE cms_block SET content=REPLACE(content,'http://aomagento.addonline.biz/','http://aomagento.jetpulp.dev/');
+UPDATE catalog_product_entity_text SET value=REPLACE(value,'http://aomagento.addonline.biz/','http://aomagento.jetpulp.dev/') WHERE attribute_id IN (97,506);
 UPDATE core_config_data SET value='mail.add-online.fr' WHERE path='system/smtp/host';
 UPDATE core_config_data SET value='1' WHERE path='dev/log/active';
 -- FIN : de local vers pr�prod
