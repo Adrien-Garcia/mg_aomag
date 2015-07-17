139473c536db8badac2c508a23995c985c5fda80
Julien GIORIA <julien.gioria@jetpulp.fr>
2015-07-17 10:53:41 +0200

    Varnish : mise a jour du FormKey des formulaires

--- server/app/code/local/Addonline/Varnish/controllers/DynController.php
+++ server/app/code/local/Addonline/Varnish/controllers/DynController.php
@@ -20,7 +20,9 @@ class Addonline_Varnish_DynController extends Mage_Core_Controller_Front_Action 
 	
 		$response = array();
 		$response['sid'] = Mage::getModel('core/session')->getEncryptedSessionId();
-	
+		// formkey nécéssaire dans tous les formulaires de la page
+		$response['formkey'] = Mage::getSingleton('core/session')->getFormKey();
+		
 		//si on visite une page produit on ajoute le produit aux dernier produit vus
 		if ($currentProductId = $this->getRequest()->getParam('currentProductId')) {
 			Mage::getSingleton('catalog/session')->setLastViewedProductId($currentProductId);
