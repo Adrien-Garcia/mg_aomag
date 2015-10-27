89cc44300f594e96adf6771fd9ce8121fe829eef
Sylvain Pras <sylvain.pras@jetpulp.fr>
2015-10-27 07:41:11 +0100

    module varnish block message désactivé à tord

--- server/app/code/local/Addonline/Varnish/Block/Core/Messages.php
+++ server/app/code/local/Addonline/Varnish/Block/Core/Messages.php
@@ -2,18 +2,15 @@
 
 class Addonline_Varnish_Block_Core_Messages extends Mage_Core_Block_Messages {
 
-    protected $_placeholder = false;
-    
     public function _prepareLayout()
     {
-        if (Mage::registry('varnish_static')) {
-            return $this;
+        if (!Mage::registry('varnish_static')) {
+            parent::_prepareLayout();
         }
-        parent::_prepareLayout();               
     }
     
     public function _toHtml(){
-        if ($this->_placeholder && Mage::registry('varnish_static')) {
+        if (Mage::registry('varnish_static')) {
             //id=BlockAlias pour pouvoir le sélectionner en javascript (sans . dans le nom), par contre on met rel=NameInLayout pour pouvoir le sélectionner dans la layout (avec . dans le nom)
             $html = '<div id="'.($this->getBlockAlias()).'" class="varnish_placeholder" rel="'.($this->getNameInLayout()).'" >'.'</div>';
         } else {
@@ -22,14 +19,4 @@ class Addonline_Varnish_Block_Core_Messages extends Mage_Core_Block_Messages {
         return $html;
     }
     
-    
-    /**
-     * Add Placeholder wrapper Flag
-     *
-     * @return void
-     */
-    public function addPlaceholder($placeholder = true)
-    {
-        $this->_placeholder = $placeholder;
-    }
 }
