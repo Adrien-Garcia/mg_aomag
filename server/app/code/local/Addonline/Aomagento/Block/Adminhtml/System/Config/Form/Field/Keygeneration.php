5584263bc29624e6af57bf231ca388fa67542377
Sylvain Pras <sylvain.pras@jetpulp.fr>
2016-02-18 16:18:52 +0100

    Upgrade module aomagento licence génération

--- server/app/code/local/Addonline/Aomagento/Block/Adminhtml/System/Config/Form/Field/Keygeneration.php
+++ server/app/code/local/Addonline/Aomagento/Block/Adminhtml/System/Config/Form/Field/Keygeneration.php
@@ -5,7 +5,7 @@ class Addonline_Aomagento_Block_Adminhtml_System_Config_Form_Field_Keygeneration
 	protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element) {
 		$_isAoMagento = Mage::getStoreConfig('addonline/licence/key_generation');
 		
-		$url = $this->getUrl('aomagento/adminhtml_generation/getlicence');
+		$url = $this->getUrl('adminhtml/aomagento_generation/getlicence');
 		
 		$html = '<input type="text" name="hostname" class="hostname" /><span style="margin-left:10px;"></span>';
 		$html .= '<input type="hidden" name="url_getlicence" class="url_getlicence" value="'.$url.'"/>';
