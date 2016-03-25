9e1edd8e604d849314b365997df3b27df2500fe9
Sylvain Pras <sylvain.pras@jetpulp.fr>
2016-03-25 17:11:00 +0100

    docker start

--- docker/start.sh
+++ docker/start.sh
@@ -7,4 +7,10 @@ export DB_BACKUP_PATTERN=addonline_aomagento_magento_*.sql.gz
 export DB_BACKUP_DIR=/opt/backup/mysql/mag5
 export DB_BACKUP_SERVER=mag5.host.addonline.fr
 
-. $(dirname "$0")/../../jetpulper/docker/start.sh
+start=$(dirname "$0")/../../jetpulper/docker/start.sh
+if [ -f $start ]
+then
+    . $start
+else
+    echo "You need to clone git@git.jetpulp.hosting:dev/jetpulper.git in the same workspace"
+fi
