64af99a239139d5ecb00529bc231b2156044e647
Sylvain Pras <sylvain.pras@jetpulp.fr>
2016-05-09 15:38:56 +0200

    factoriser variable environnement SERVER_NAME dans conf docker

--- docker/start.sh
+++ docker/start.sh
@@ -6,6 +6,7 @@
 export DB_BACKUP_PATTERN=theme_mag_20*.sql.gz
 export DB_BACKUP_DIR=/vol/nfs_backup_sql/mag-db3-new/mysql/mag-db3-new
 export DB_BACKUP_SERVER=aotools.host.addonline.fr
+export SERVER_NAME=mg-aomagento.$JETPULP_USERNAME.jetpulp.dev
 
 start=$(dirname "$0")/../../jetpulper/docker/start.sh
 if [ -f $start ]
