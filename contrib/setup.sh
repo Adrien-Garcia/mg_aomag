1bfcf097b489f0c17bd4731a569930982c6eeb2b
Sylvain Pras <sylvain.pras@jetpulp.fr>
2016-08-24 18:06:28 +0200

    Blocage deploy benstalk, rename readme

--- contrib/setup.sh
+++ contrib/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-echo "Copy pre-commit"
-cp contrib/pre-commit .git/hooks/pre-commit
-chmod +x .git/hooks/pre-commit
-
-echo "Copy pre-push"
-cp contrib/pre-push .git/hooks/pre-push
-chmod +x .git/hooks/pre-push
+#echo "Copy pre-commit"
+#cp contrib/pre-commit .git/hooks/pre-commit
+#chmod +x .git/hooks/pre-commit
+#
+#echo "Copy pre-push"
+#cp contrib/pre-push .git/hooks/pre-push
+#chmod +x .git/hooks/pre-push
 
 echo "Add magento coding standard to PHPCS path"
 ./vendor/bin/phpcs --config-set installed_paths ../../magento-ecg/coding-standard
